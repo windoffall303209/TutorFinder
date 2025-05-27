86c7331b0ad45bf666528334b88904c653996aaf
WIND OF FALL <nvuthanh4@gmail.com>
2025-05-27 19:53:48 +0700

    test deloy 1

--- config/db.js
+++ config/db.js
@@ -1,10 +1,21 @@
-const mysql = require("mysql2/promise");
+const { Sequelize } = require('sequelize');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
 
-module.exports = pool;
+module.exports = sequelize;
